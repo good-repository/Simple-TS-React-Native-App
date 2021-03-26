3f4036a1b9572327267c8f46a7fc1fc522656d62
Guilheme Fünkler Borelli <borelli.guilherme@gmail.com>
2021-03-26 09:43:43 -0300

    Add Saga to the Store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,21 @@
-import {createStore, Store} from 'redux';
+import {createStore, applyMiddleware, Store} from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import {UsersState} from './users/types';
 
 import rootReducer from './rootReducer';
+import rootSaga from './rootSaga';
 
 export interface ApplicationState {
   users: UsersState;
 }
 
-const store: Store<ApplicationState> = createStore(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
+
+const store: Store<ApplicationState> = createStore(
+  rootReducer,
+  applyMiddleware(sagaMiddleware),
+);
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
